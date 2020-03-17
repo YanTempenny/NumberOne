8a447b4af417fa99bc142da992e49022a711e69e
Gregory <60688203+YanTempenny@users.noreply.github.com>
2020-03-17 14:21:13 +0300

    Update V2.sql

--- V2.sql
+++ V2.sql
@@ -4,7 +4,7 @@ IF OBJECT_ID('dbo.Workers', 'U') IS NOT NULL DROP TABLE dbo.Workers;
 CREATE TABLE dbo.Workers (
 WorkersID BIGINT IDENTITY(1,1) CONSTRAINT PK_WorkersId PRIMARY KEY,
 FullName Varchar (100) NULL,
-Age INT (3) NULL,
+Age INT NULL,
 Gender VARCHAR (25) NULL,
 Adress VARCHAR (100) NOT NULL,
 Number INT  NULL,

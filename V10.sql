20e2e7886b33a99b936a3f965e9e1c65e08b353f
Gregory <60688203+YanTempenny@users.noreply.github.com>
2020-03-26 17:30:00 +0300

    Add files via upload

---
+++ V10.sql
@@ -0,0 +1,13 @@
+USE Restauraunt;
+
+INSERT INTO dbo.Workers (FullName, Age, Gender, Adress, Number, PassportDate, CodePosition) VALUES
+('John Tempenny', 21, 'Male', 'UK London', 12825245, 124578, 1),
+('Bendy Tempenny', 19, 'Male', 'RU Moskva', 88002587, 968574, 2),
+('Clara Emirsson', 38, 'Female', 'BY Minsk', 65854523, 852741, 3),
+('Amanda Jackson', 25, 'Female', 'USA New York', 2452156, 321654, 4),
+('Jason Henderson', 30, 'Male', 'UK Liverpool', 1587855, 258789, 5),
+('Bane Versace', 22, 'Male', 'UK London', 11125478, 852123, 6),
+('Dereck Santiago', 32, 'Male', 'AF Kabul', 2552689, 123789, 7),
+('Gregory Tempenny', 27, 'Male', 'BR Goiania', 4657891, 753951, 8),
+('Sofia Abramova', 23, 'Female', 'RU Belgorod', 89155552, 5554786, 9),
+('Olivia Redmond', 34, 'Female', 'IN Ahmedabad', 999647555, 254159, 10);

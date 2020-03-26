cad7e8ecee24d274d0fd296e64dd90153d7e52bb
Gregory <60688203+YanTempenny@users.noreply.github.com>
2020-03-26 16:53:32 +0300

    Add files via upload

---
+++ V9.sql
@@ -0,0 +1,13 @@
+USE Restauraunt;
+
+INSERT INTO dbo.Warehouse (NameIngredient, DateIssue, Volume, ShelfLife, Cost, Supplier)  VALUES 
+('Salad', '2020-02-29', 200, 15, '5000.00', 'MST'),
+('Soup', '2020-01-30', 500, 30, '10500.00', 'WinterTM'),
+('Steak', '2019-11-15', 2000, 55, '8750.00', 'MST'),
+('Ice Cream', '2019-05-06', 700, 80, '4000.00', 'HappySmile'),
+('Sushi', '2019-07-01', 5000, 30, '9000.00', 'WinterTM'),
+('Bread', '2018-02-01', 1700, 15, '1000.00', 'MST'),
+('Spaghetti', '2019-08-15', 3500, 25, '11000.00', 'Qwerty'),
+('Water', '2018-01-30', 1500, 90, '4000.00', 'WinterTM'),
+('Wine', '2019-07-18', 1600, 60, '7750.00', 'RadD'),
+('Juice', '2020-03-02', 1900, 60, '5250.00', 'RadD');
